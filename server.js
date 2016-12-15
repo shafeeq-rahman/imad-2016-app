2da6c7790b4ff5b310f998318379dc3a5b3b1655
shafeeq-rahman <shafeeqrahman96@gmail.com>
2016-12-15 20:37:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ var app = express();
 app.use(morgan('combined'));
 
 
-var articleOne = {
+var articles = { 
+    'article-one': {
   title: 'Article One | Shafeeq',
   heading: 'Article One',
   date: 'December 10',
@@ -20,8 +21,38 @@ var articleOne = {
             <p>
                 This is the content for my first article.
             </p>`
+},
+    'article-two': {
+         title: 'Article Two | Shafeeq',
+  heading: 'Article Two',
+  date: 'December 10',
+  content: `
+            <p>
+                This is the content for my second articles.
+            </p>
+            <p>
+                This is the content for my second article.
+            </p>
+            <p>
+                This is the content for my second article.
+            </p>`
+    },
+    'article-three': {
+         title: 'Article Three | Shafeeq',
+  heading: 'Article Three',
+  date: 'December 10',
+  content: `
+            <p>
+                This is the content for my third articles.
+            </p>
+            <p>
+                This is the content for my third article.
+            </p>
+            <p>
+                This is the content for my third article.
+            </p>`
+    },
 };
-
 function createTemplate (data) {
     var title = data.title;
     var date = data.date;
@@ -68,7 +99,11 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res) {  res.send(createTemplate(articleOne));
+app.get('/:articleName', function (req, res) {
+    //articleName= article-one
+    // article[articleName]= {} content object for article one //
+    var articleName = req.params.articleName; // to access the articleName parameter//
+    res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/article-two', function (req, res) { res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
