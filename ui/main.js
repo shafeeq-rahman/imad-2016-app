1e1528e3ea36868a21cf95bac9d2187a273bc4bc
shafeeq-rahman <shafeeqrahman96@gmail.com>
2016-12-15 22:45:08 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,6 +7,6 @@ element.innerHTML = "New Value";
 
 //move the image
 var img= document.getElementById('madi');
-img.onclick= function(){
+img.onclick = function(){
     img.style.marginLeft = '100px';
 };
