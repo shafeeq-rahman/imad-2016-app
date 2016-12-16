be480ee37db8e2a400d39b5ff5c2e6d8094a0e1d
shafeeq-rahman <shafeeqrahman96@gmail.com>
2016-12-16 20:57:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,14 +9,9 @@ button.onclick = function(){
   request.onreadystatechange = function(){
     if(request.readyState === XMLHttpRequest.DONE){
         if(request.status === 200){
-           var names=request.responseText;
-           names= JSON.parse(names);
-           var list = '';
-           for(var i=0; i< names.length;i++){
-               list = '<li>'+names[i]+'</li>';
-           }
-           var ul = document.getElementById('namelist');
-           ul.innerHTML=list;
+            var counter = request.responseText;
+            var span = document.getElementById('count');
+            span.innerHTML = counter.toString();
         }
     }  
   };
@@ -35,13 +30,18 @@ submit.onclick = function(){
   request.onreadystatechange = function(){
     if(request.readyState === XMLHttpRequest.DONE){
         if(request.status === 200){
-            var counter = request.responseText;
-            var span = document.getElementById('count');
-            span.innerHTML = counter.toString();
+            var names = request.responseText;
+            names = JSON.parse(names);
+            var list='';
+            for(var i=0; i<names.length;i++){
+                list = '<li>' + names[i] + '</li>';
+            }
+            var ul = document.getElementById('namelist');
+            ul.innerHTML =list;
         }
     }  
   };
   
-  request.open('GET','http://shafeeq-rahman.imad.hasura-app.io/submit-name?name='+name,true);
+  request.open('GET','http://shafeeq-rahman.imad.hasura-app.io/submit-name?name=',true);
   request.send(null);
 };
