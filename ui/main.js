457d9198af3c20be38be63ec640a13f8b20a6b90
shafeeq-rahman <shafeeqrahman96@gmail.com>
2016-12-16 21:56:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -42,7 +42,7 @@ submit.onclick = function(){
   };
   
   var nameInput = document.getElementById('name');
-var name = nameInput.value;
-  request.open('GET','http://shafeeq-rahman.imad.hasura-app.io/submit-name?name=',true);
+  var name = nameInput.value;
+  request.open('GET','http://shafeeq-rahman.imad.hasura-app.io/submit-name?name=' + name,true);
   request.send(null);
 };
