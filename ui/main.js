a550ee8d25eccd5551c1e73698f7469fe7c53b78
shafeeq-rahman <shafeeqrahman96@gmail.com>
2016-12-16 19:07:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,6 +17,6 @@ button.onclick = function(){
     }  
   };
   
-  request.open('GET','http://shafeeq-rahman.imad.hausra-app.io',true);
+  request.open('GET','http://shafeeq-rahman.imad.hausra-app.io/counter',true);
   request.send(null);
 };
